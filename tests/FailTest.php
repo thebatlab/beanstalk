6173df5fe6deddf525ff5eed7c459885e417f7ee
shanegiroux <shane.giroux@zu.com>
2022-07-19 14:12:02 -0600

    add a simple failing test case to see what happens in build

---
+++ tests/FailTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class FailTest extends KernelTestCase
+{
+
+    public function testFailCase()
+    {
+        self::assertEquals(false, true);
+    }
+}
